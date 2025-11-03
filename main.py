5ede56cc8350cbafc040cb9fc1fd59558ce438cb
leettt678-code <leettt678@gmail.com>
2025-11-03 13:02:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
 st.title('우거지 해장국과 실존하는 진우')
-st.text_input('이름을 입력하세요:')
-st.button('인사말 생성')
+name=st.text_input('이름을 입력하세요:')
+if st.button('인사말 생성'):
+  st.write(name+'님! 안녕하세요')
